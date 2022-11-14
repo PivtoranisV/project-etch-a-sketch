3b3de6921cf5c8a48609f1edb7bcf7cd542f79cd
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-11-14 11:44:52 +0400

    added functionality to choose board size

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const board = document.querySelector('.board');
+const boardSize = document.querySelector('input');
 
-makeBoard(32);
+boardSize.addEventListener('change', (event) => makeBoard(event));
 
-function makeBoard(size) {
+function makeBoard(event) {
+  const size = Number(event.target.value);
+  board.replaceChildren();
+
   for (let i = 0; i < size * size; i++) {
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     const cell = document.createElement('div');
+    cell.addEventListener('mouseenter', () => {
+      cell.style.backgroundColor = 'blue';
+    });
     cell.classList.add('cell');
     board.appendChild(cell);
   }
 }
-
-const cells = document.querySelectorAll('.cell');
-
-cells.forEach((cell) =>
-  cell.addEventListener('mouseenter', () => {
-    cell.style.backgroundColor = 'blue';
-  })
-);
